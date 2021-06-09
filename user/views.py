2eb72ac0177bdf40a169e41804a0f950c81bd18e
Hardik   Rathod <38103307+hr4official@users.noreply.github.com>
2021-06-09 15:38:10 +0530

    login design added

--- user/views.py
+++ user/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate
 def homepage(request):
     return render(request,"homepage.html")
 
 def login(request):
-    return render(request,"login.html")
+    
+    if request.method  == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, username=username , password=password)
+        
+        if user is not None:
+            login(request,user)
+            return redirect('homepage')
+        else:
+            messages.info(request,'username or password is wrong')
+            
+    context = {}
+    return render(request,"login.html",context)
